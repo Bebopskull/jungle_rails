a325014ff43c069a9dededc5c7ed248333037cc4
Nelson Eduardo Vasquez <bebopskull@gmail.com>
2021-03-04 16:26:02 -0500

    basic dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,10 @@
 class Admin::DashboardController < ApplicationController
+  	http_basic_authenticate_with name: "jungle", password: "book"
+
+
   def show
+    @products_count = Product.count
+ 		@categories_count = Category.count
   end
 end
 
